71098b53f56b34e855af153cefc2e954bbd12e49
student_2333 <lgc2333@126.com>
2024-05-15 16:53:56 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,17 @@ export interface Config {}
 export const Config: z<Config> = z.object({})
 
 export function apply(ctx: Context) {
-  ctx.console.addEntry({
-    dev: resolve(__dirname, '../client/index.ts'),
-    prod: resolve(__dirname, '../dist'),
-  })
+  ctx.console.addEntry(
+    process.env.KOISHI_BASE
+      ? [
+          process.env.KOISHI_BASE + '/dist/index.js',
+          process.env.KOISHI_BASE + '/dist/style.css',
+        ]
+      : process.env.KOISHI_ENV === 'browser'
+        ? [import.meta.url.replace(/\/src\/[^/]+$/, '/client/index.ts')]
+        : {
+            dev: resolve(__dirname, '../client/index.ts'),
+            prod: resolve(__dirname, '../dist'),
+          },
+  )
 }
